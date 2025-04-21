cef67afb7009aaa150c80477112754cef41411b3
Henrique <henry2camargo@gmail.com>
2025-04-20 21:11:58 -0300

    Refactor Header

--- src/components/Shared/layout/Header/Header.jsx
+++ src/components/Shared/layout/Header/Header.jsx
@@ -1,8 +1,21 @@
-import Header from './Header.module.css';
-import React from 'react';
+import React, { useState } from 'react';
 import { FaUserCircle, FaCut } from 'react-icons/fa';
+import Header from './Header.module.css';
+import { useNavigate } from 'react-router-dom'; 
 
 const HeaderComponent = () => {
+    const [showOptions, setShowOptions] = useState(false);
+    const navigate = useNavigate(); 
+
+    const toggleOptions = () => {
+        setShowOptions(!showOptions);
+    };
+
+    const logout = () => {
+        console.log("Saindo do sistema...");
+        navigate('/login'); // Redireciona para a página de login
+    };
+
     return (
         <header className={Header.header}>
             <div className={Header.logo}>
@@ -10,7 +23,14 @@ const HeaderComponent = () => {
                 <span>HairFlow</span>
             </div>
             <div className={Header.profile}>
-                <FaUserCircle className={Header.profileIcon} />
+                <button className={Header.profileButton} onClick={toggleOptions}>
+                    <FaUserCircle className={Header.profileIcon} />
+                </button>
+                {showOptions && (
+                    <div className={Header.optionsMenu}>
+                        <button className={Header.option} onClick={logout}>Sair</button>
+                    </div>
+                )}
             </div>
         </header>
     );
