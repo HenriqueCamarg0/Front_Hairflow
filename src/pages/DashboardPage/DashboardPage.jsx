832e1646e442aabc438f687f5d67c445319226bd
Henrique <henry2camargo@gmail.com>
2025-04-27 08:57:52 -0300

    Add scheduling page and optimize user experience with design improvements

--- src/pages/DashboardPage/DashboardPage.jsx
+++ src/pages/DashboardPage/DashboardPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Container from '../../components/Shared/Layout/Container/Container';
 import styles from './DashboardPage.module.css'; // Importando o CSS do DashboardPage
 
